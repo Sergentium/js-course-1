503946189a0a306379fd3c8e1730dc1784f0a48f
segentium.info <slpetrenko102@gmail.com>
2022-05-05 18:43:56 +0300

    First commit

---
+++ variables.js
@@ -0,0 +1,32 @@
+console.log(student); //undefined
+
+var student = "Serhii"
+console.log(student);
+
+var count;
+console.log(count);//undefined
+
+count = 1;
+console.log(count); // 1
+count = 2;
+console.log(count); // 2
+
+// console.log(number); // Uncaught ReferenceError: Cannot access 'number' before initialization
+let number = 30;
+console.log(number);
+
+let age;
+console.log(age); //unefined
+
+age = 18;
+console.log(18);
+
+age = 22;
+console.log(age);
+
+const userName = "Vova";
+console.log(userName);
+
+
+
+// userName = ""; //variables.js:32 Uncaught TypeError: Assignment to constant variable.
